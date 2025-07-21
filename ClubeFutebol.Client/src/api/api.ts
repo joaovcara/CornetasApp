9acde390a91fc27191dfc9899a64231f341f7d0d
joao.cara <joao.cara@outlook.com.br>
2025-07-21 20:45:42 -0300

    Envio do token em requisições

--- ClubeFutebol.Client/src/api/api.ts
+++ ClubeFutebol.Client/src/api/api.ts
@@ -8,4 +8,15 @@ const api = axios.create({
   },
 });
 
+// Interceptor para adicionar o token em todas as requisições
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
 export default api;
